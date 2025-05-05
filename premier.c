726778722d1b373692e544216f5d95202477141c
Olivier <oliviernadeau2003@gmail.com>
2025-05-05 00:04:33 -0400

    .

--- premier.c
+++ premier.c
@@ -6,10 +6,10 @@ int main()
     for (nbr = 0; nbr < 100; nbr++)
     {
         for (i = 1; i <= nbr; i++)
-            //                                                                                                                                 nbr % i ?: p++;
+            // nbr % i ?: p++;
             if (nbr % i == 0)
                 p++;
-        //                                                                                                                                    p == 2 ? printf(" %d est un nombre premier.\n", nbr) : 0;
+        // p == 2 ? printf(" %d est un nombre premier.\n", nbr) : 0;
         if (p == 2)
             printf(" %d est un nombre premier.\n", nbr);
         p = 0;
